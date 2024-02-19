d12226b8cbaf3098c12a0910acd98675d45f045d
YhonatanGoomez <90011314+YhonatanGoomez@users.noreply.github.com>
2024-02-19 18:49:36 -0500

    Update StartProduction.java

--- src/main/java/edu/eci/arst/concprg/prodcons/StartProduction.java
+++ src/main/java/edu/eci/arst/concprg/prodcons/StartProduction.java
@@ -16,26 +16,26 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class StartProduction {
-    
-    
+
+
     public static void main(String[] args) {
-        
+
         Queue<Integer> queue=new LinkedBlockingQueue<>();
 
         long stockLimit = 500;
 
         new Producer(queue,stockLimit).start();
-        
+
         //let the producer create products for 5 seconds (stock).
         try {
             Thread.sleep(5000);
         } catch (InterruptedException ex) {
             Logger.getLogger(StartProduction.class.getName()).log(Level.SEVERE, null, ex);
         }
-        
-        
+
+
         new Consumer(queue).start();
     }
-    
+
 
 }
